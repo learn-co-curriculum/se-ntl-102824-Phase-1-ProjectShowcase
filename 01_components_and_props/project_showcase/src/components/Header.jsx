eddc596f1fbe74772a0385625f28edea6e3d2fca
Laura Berge <lauraeberge@gmail.com>
2024-11-18 13:03:23 -0600

    create Header

---
+++ 01_components_and_props/project_showcase/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1><span className="logo">//</span>Project Showcase</h1>
+            <button>Light Mode</button>
+        </header>
+    )
+}
+
+export default Header;
